fa985f3bda327c3769f54f42a95ef15895fc3645
tappily <tappily.b@gmail.com>
2014-07-11 08:58:08 -0700

    Adding .pin.guide. Adding autoprefixer.

--- Gruntfile.js
+++ Gruntfile.js
@@ -134,7 +134,7 @@ module.exports = function(grunt) {
       },
       less: {
         files: 'src/less/**/*.less',
-        tasks: ['less']
+        tasks: ['less', 'newer:autoprefixer:site']
       },
       template: {
         files: 'src/templates/**/*.{js,hbs}',
